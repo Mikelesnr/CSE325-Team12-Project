1c6f4e805e8fd9875701477872a92f02c8eaff66
Menocas17 <rodolfomenocal7@gmail.com>
2025-10-05 16:25:11 -0600

    -Created the button component -Created the main layout of the app

---
+++ Components/ButtonEnums.cs
@@ -0,0 +1,19 @@
+namespace Project.Components
+{
+    public enum ButtonVariant
+    {
+        Primary,
+        Secondary,
+        Danger,
+        Empty
+    }
+
+    public enum ButtonSize
+    {
+        Small,
+        Medium,
+        Large,
+
+
+    }
+}
