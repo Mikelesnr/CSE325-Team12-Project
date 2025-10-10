12f106b08c54efa14b5b0f35d338cedf026fef50
Michael Mwanza <mwanza.n.m@gmail.com>
2025-10-10 04:28:45 +0200

    chat views created

--- Services/ConversationService.cs
+++ Services/ConversationService.cs
@@ -13,12 +13,29 @@ namespace CSE325_Team12_Project.Services
             _context = context;
         }
 
-        public async Task<Conversation> CreateConversationAsync(List<Guid> userIds)
+        public async Task<Conversation> CreateConversationAsync(List<Guid> userIds, Guid createdBy)
         {
+            if (userIds.Count == 2)
+            {
+                var existing = await _context.Conversations
+                    .Include(c => c.Participants)
+                    .Where(c => !c.IsGroup &&
+                                c.Participants.Count == 2 &&
+                                userIds.All(uid => c.Participants.Any(p => p.UserId == uid)))
+                    .FirstOrDefaultAsync();
+
+                if (existing != null)
+                {
+                    return existing;
+                }
+            }
+
             var conversation = new Conversation
             {
                 Id = Guid.NewGuid(),
+                CreatedBy = createdBy,
                 CreatedAt = DateTime.UtcNow,
+                IsGroup = userIds.Count > 2,
                 Participants = userIds.Select(id => new ConversationParticipant
                 {
                     UserId = id,
--- Services/IConversationService.cs
+++ Services/IConversationService.cs
@@ -4,7 +4,7 @@ namespace CSE325_Team12_Project.Services
 {
     public interface IConversationService
     {
-        Task<Conversation> CreateConversationAsync(List<Guid> userIds);
+        Task<Conversation> CreateConversationAsync(List<Guid> userIds, Guid createdBy);
         Task<ConversationParticipant> AddParticipantAsync(Guid conversationId, Guid userId);
     }
 }
